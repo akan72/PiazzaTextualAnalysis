20a19817820d41380d5d2ec674d285f47e4c6fe7
Alex Kan <lexokan@gmail.com>
2019-11-10 21:52:32 -0500

    Update requirements.txt and add build script

---
+++ piazzatextualanalysis/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check to see if the two Piazza login environment variables are set
+if [[ -z "${PIAZZA_EMAIL}" ]]; then
+	echo "PIAZZA_EMAIL is not set!"
+	exit 1
+fi
+
+if [[ -z "${PIAZZA_PASSWORD}" ]]; then
+    echo "PIAZZA_PASSWORD is not set!"
+	exit 1
+fi
+
+# Install Python package requirements
+pip install -r requirements.txt
+
+# Run the jupyter notebook pipeline
+jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=300 pickle_posts.ipynb
+jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=300 transform_posts.ipynb
